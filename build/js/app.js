652618af074e493145cae300102d82eb1add8449
Jaran F <jaranMcLarn@gmail.com>
2017-07-06 07:19:38 +0100

    Refactor

--- build/js/app.js
+++ build/js/app.js
@@ -2,17 +2,16 @@
 /*jslint newcap: true, white: true, sloppy: true, vars: true, unparam: true */
 
 angular.module('myApp',[])
-    .controller('MainCtrl', [function() {
-            var self = this;
-            self.i8nMessages = {
-                quoteBoxMsg: '...Oh Yes!',
-                promoBenefits: [
-                    {reason: 'We believe in being there for you.'},
-                    {reason: 'It\'s important that we live this through our policies.'},
-                    {reason: 'We believe that every interaction matters.'},
-                    {reason: 'We spend time making sure you\'re happy. Oh Yes!'}
-                ]
-            };
-            self.quoteBoxMsg = '...Oh Yes!';
-        }]
+      .controller('MainCtrl', [function() {
+              var self = this;
+              self.i8nMessages = {
+                  quoteBoxMsg: '...Oh Yes!',
+                  promoBenefits: [
+                      {reason: 'We believe in being there for you.'},
+                      {reason: 'It\'s important that we live this through our policies.'},
+                      {reason: 'We believe that every interaction matters.'},
+                      {reason: 'We spend time making sure you\'re happy. Oh Yes!'}
+                  ]
+              };
+          }]
     );
